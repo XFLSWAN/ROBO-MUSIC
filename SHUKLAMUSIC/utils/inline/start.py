50dffd8549c9549b77644f22b7c40366dc178250
˹sᴡᴧɴ ꭙ 𝙲𝚎𝚖𝚊𝚛𝚊⁹⁹˼ 🇮🇩 🇮🇳 <158449266+XFLSWAN@users.noreply.github.com>
2024-06-04 16:29:55 +0530

    Update start.py

--- SHUKLAMUSIC/utils/inline/start.py
+++ SHUKLAMUSIC/utils/inline/start.py
@@ -10,7 +10,7 @@ def start_panel(_):
             InlineKeyboardButton(
                 text=_["S_B_1"], url=f"https://t.me/{app.username}?startgroup=true"
             ),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
+            
         ],
     ]
     return buttons
@@ -25,10 +25,7 @@ def private_panel(_):
             )
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
-        [
-            InlineKeyboardButton(text=_["S_B_6"], url=config.SUPPORT_CHANNEL),
-            InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
-        ],
+  
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
         ],
