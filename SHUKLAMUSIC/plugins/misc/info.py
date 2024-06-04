1b93c1095ad62941cc0450b3160d40432c1d3a35
˹sᴡᴧɴ ꭙ 𝙲𝚎𝚖𝚊𝚛𝚊⁹⁹˼ 🇮🇩 🇮🇳 <158449266+XFLSWAN@users.noreply.github.com>
2024-06-04 15:55:50 +0530

    Update info.py

--- SHUKLAMUSIC/plugins/misc/info.py
+++ SHUKLAMUSIC/plugins/misc/info.py
@@ -111,7 +111,7 @@ async def userstatus(user_id):
 
 
 
-@app.on_message(filters.command(["info", "userinfo"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]))
+@app.on_message(filters.command(["minfo", "userinfo"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]))
 async def userinfo(_, message):
     chat_id = message.chat.id
     user_id = message.from_user.id
